b9fae3dfaad03c30700ecf28b85e94a12a894caf
Sehan Munir <sehanmunirgoldar@gmail.com>
2026-02-17 11:16:57 -0500

    Refactor Basics

--- SumoBot/sumobot.ino
+++ SumoBot/sumobot.ino
@@ -5,142 +5,95 @@
 // Sumo Bot
 
 // Imports
-#include <bits/stdc++.h>
-#include "Adafruit_VL53L0X.h"
+#include <Wire.h>
+#include <Adafruit_VL53L0X.h>
 
 // Motors
-#define R_MOTOR_PWM 3 // PWMA
-#define R_MOTOR_P1 4  // AIN1
-#define R_MOTOR_P2 5  // AIN2
+  // Right Motor
+#define R_MOTOR_PWM 5 // PWMA
+#define R_MOTOR_P1 7 // AIN1
+#define R_MOTOR_P2 8 // AIN2
+  // Left Motor
 #define L_MOTOR_PWM 6 // PWMB
-#define L_MOTOR_P1 7  // BIN1
-#define L_MOTOR_P2 8  // BIN2
-#define MOTOR_STBY 9  // STBY
+#define L_MOTOR_P1 9 // BIN1
+#define L_MOTOR_P2 10 // BIN2
 
 // Ground Sensors
 #define LDR_F A0
 #define LDR_B A1
 
 // Distance Sensors (VL53L0X Dual Sensor Setup)
-#define LOX1_ADDRESS 0x30
-#define LOX2_ADDRESS 0x31
-#define SHT_LOX1 10
-#define SHT_LOX2 11
+  //
+Adafruit_VL53L0X loxR = Adafruit_VL53L0X();
+Adafruit_VL53L0X loxL = Adafruit_VL53L0X();
+  // 
+#define LOXR_SHT 2
+#define LOXL_SHT 3
+  //
+#define LOXR_ADDRESS 0x30
+#define LOXL_ADDRESS 0x31
+// Distance Sensor (The other one)
 
-// VL53L0X sensor objects
-Adafruit_VL53L0X lox1 = Adafruit_VL53L0X();
-Adafruit_VL53L0X lox2 = Adafruit_VL53L0X();
+// Additional Constants
+#define SPIN_SPEED 0.9 // Spin Speed Percentage
+#define DIST_DETECT_THRESHOLD 850 // 
+#define LDR_DETECT_THRESHOLD 250 // 
 
-// Measurement data structures
-VL53L0X_RangingMeasurementData_t measure1;
-VL53L0X_RangingMeasurementData_t measure2;
 
-#define BASE_SPEED 150 // 1 - 255
-#define DETECT_THRESHOLD 300
-#define LDR_CONSTRAINT 600
-
-void setID() {
-  digitalWrite(SHT_LOX1, LOW);
-  digitalWrite(SHT_LOX2, LOW);
+// Weird method required for setting multiple VL52L0X
+  // Start with both off, restart one sensor at one memory address, and restart the other at another memoory address via XSHUT pin
+void setVL52L0X() {
+  pinMode(LOXR_SHT, OUTPUT);
+  pinMode(LOXL_SHT, OUTPUT);
+  // Off
+  digitalWrite(LOXR_SHT, LOW);
+  digitalWrite(LOXL_SHT, LOW);
   delay(10);
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, HIGH);
+  // Restart R
+  digitalWrite(LOXR_SHT, HIGH);
   delay(10);
-  // activating LOX1 and resetting LOX2
-  digitalWrite(SHT_LOX1, HIGH);
-  digitalWrite(SHT_LOX2, LOW);
-
-  // initing LOX1
-  if (!lox1.begin(LOX1_ADDRESS)) {
-    Serial.println(F("Failed to boot first VL53L0X"));
-    while (1);
-  }
+  loxR.begin(LOXR_ADDRESS);
+  // Restart L
+  digitalWrite(LOXL_SHT, HIGH);
   delay(10);
-  // activating LOX2
-  digitalWrite(SHT_LOX2, HIGH);
-  delay(10);
-  // initing LOX2
-  if (!lox2.begin(LOX2_ADDRESS)) {
-    Serial.println(F("Failed to boot second VL53L0X"));
-    while (1);
-  }
-}
-
-void read_dual_sensors() {
-  lox1.rangingTest(&measure1, false); // pass in 'true' to get debug data printout!
-  lox2.rangingTest(&measure2, false); // pass in 'true' to get debug data printout!
-  // Sensor 1 
-  Serial.print(F("1: "));
-  if (measure1.RangeStatus != 4) { // if not out of range
-    Serial.print(measure1.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }
-  Serial.print(F(" "));
-  // Sensor 2
-  Serial.print(F("2: "));
-  if (measure2.RangeStatus != 4) {
-    Serial.print(measure2.RangeMilliMeter);
-  } else {
-    Serial.print(F("Out of range"));
-  }
-  Serial.println();
+  loxL.begin(LOXL_ADDRESS);
+  Serial.println("VL52L0X Set");
 }
 
 void setup() {
+  Serial.begin(9600);
+  setVL52L0X();
   pinMode(R_MOTOR_P1, OUTPUT);
   pinMode(R_MOTOR_P2, OUTPUT);
   pinMode(L_MOTOR_P1, OUTPUT);
   pinMode(L_MOTOR_P2, OUTPUT);
-
   pinMode(R_MOTOR_PWM, OUTPUT);
   pinMode(L_MOTOR_PWM, OUTPUT);
-  pinMode(MOTOR_STBY, OUTPUT);
-
-  digitalWrite(MOTOR_STBY, HIGH);
-
-  Serial.begin(115200);
-  while (!Serial) {
-    delay(1);
-  }
-
-  pinMode(SHT_LOX1, OUTPUT);
-  pinMode(SHT_LOX2, OUTPUT);
-  Serial.println(F("Shutdown pins initialized..."));
-  digitalWrite(SHT_LOX1, LOW);
-  digitalWrite(SHT_LOX2, LOW);
-  Serial.println(F("Both sensors in reset mode...(pins are low)"));
-  Serial.println(F("Starting dual VL53L0X sensor initialization..."));
-  setID();
-  Serial.println(F("VL53L0X sensors initialized successfully!"));
+  Serial.println("Setup Finished");
 }
 
 void loop() {
   // Read dual VL53L0X sensors
-  read_dual_sensors();
-  int distL = (measure1.RangeStatus != 4) ? measure1.RangeMilliMeter : 5000;
-  int distR = (measure2.RangeStatus != 4) ? measure2.RangeMilliMeter : 5000;
-  
-  int ldrF = analogRead(LDR_F);
-  int ldrB = analogRead(LDR_B);
-  
-  bool frontWhite = ldrF > LDR_CONSTRAINT;
-  bool backWhite = ldrB > LDR_CONSTRAINT;
+  VL53L0X_RangingMeasurementData_t measureLOXR;
+  VL53L0X_RangingMeasurementData_t measureLOXL;
 
-  if (frontWhite && backWhite) { // pretty much lost senario
-    motors(BASE_SPEED, -BASE_SPEED);
-  } else if (frontWhite) {
-    escapeB();
-  } else if (backWhite) {
-    escapeF();
+  loxR.rangingTest(&measureLOXR, false);
+  loxL.rangingTest(&measureLOXL, false);
+  
+  int distR = measurementCentimeters(measureLOXR.RangeMilliMeter);
+  int distL = measurementCentimeters(measureLOXL.RangeMilliMeter);
+  // Excape Conditions
+  if (distR > DIST_DETECT_THRESHOLD || distL > DIST_DETECT_THRESHOLD){
+    // where tf is ts bot
+    search();
+  } else if (distR < distL+2){
+    motors(motorSpeed(0.2), motorSpeed(0.1));
+  } else if (distR+2 > distL){
+    motors(motorSpeed(0.1), motorSpeed(0.2)):
   } else {
-    if (distL < DETECT_THRESHOLD || distR < DETECT_THRESHOLD) {
-      stopMotors();
-    } else {
-      spin();
-    }
+    motors(motorSpeed(0.3), motorSpeed(0.3));
   }
-  delay(100);
+  delay(10);
 }
 
 void motors(int right_speed, int left_speed) {
@@ -172,18 +125,14 @@ void stopMotors() {
   motors(0, 0);
 }
 
-void spin() {
-  motors(BASE_SPEED, -BASE_SPEED);
-}
-
-void escapeF() {
-  motors(BASE_SPEED, BASE_SPEED);
-}
-
-void escapeB() {
-  motors(-BASE_SPEED, -BASE_SPEED);
+void search() {
+  motors(motorSpeed(SPIN_SPEED), motorSpeed(SPIN_SPEED));
 }
 
 int motorSpeed(double percentage) {
   return (int)(percentage*(255));
+}
+
+double measurementCentimeters(int millimeter){
+  return (millimeter)/10;
 }
