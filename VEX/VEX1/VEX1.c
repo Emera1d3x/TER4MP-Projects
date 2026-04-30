db0acb9103b20afcbbc249ca6d5774e2487589ec
Sehan Munir <sehanmunirgoldar@gmail.com>
2026-04-30 13:40:00 -0400

    Update VEX1.c

--- VEX/VEX1/VEX1.c
+++ VEX/VEX1/VEX1.c
@@ -8,8 +8,8 @@ task main()
 {
     while (true) {
         int forward = vexRT[Ch3]; // left joystick Y
-				int turn    = vexRT[Ch1]; // right joystick X
-        //motor[TR] = forward;
+				int turn    = vexRT[Ch1]*(0.8); // right joystick X
+        motor[TR] = forward-turn;
         motor[TL] = -forward-turn;
         motor[BR] = forward-turn;
         motor[BL] = -forward-turn;
